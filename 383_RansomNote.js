c4e373269c98777ef7cc62e732b9a3745b3ae9a0
Kaung Khant <kksh765@gmail.com>
2026-06-30 16:34:56 +0630

    383 wip

---
+++ 383_RansomNote.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+ransomNote = "a";
+magazine = "b";
+
+var canConstruct = function (ransomNote, magazine) {};
