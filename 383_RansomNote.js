5fee243c55c5b8cb82a76a5286262959b56b320f
Kaung Khant <kksh765@gmail.com>
2026-07-13 13:27:19 +0630

    383 solved

--- 383_RansomNote.js
+++ 383_RansomNote.js
@@ -4,6 +4,26 @@
  * @return {boolean}
  */
 ransomNote = "a";
-magazine = "b";
+magazine = "ab";
 
-var canConstruct = function (ransomNote, magazine) {};
+var canConstruct = function (ransomNote, magazine) {
+  const inventory = {};
+
+  //(Logical OR) The || operator looks at the value on its left.
+  // First time seeing a letter: undefined || 0 evaluates to 0.
+  // Second time seeing a letter: 1 || 0 evaluates to 1.
+  for (let char of magazine) {
+    inventory[char] = (inventory[char] || 0) + 1;
+  }
+
+  for (let char of ransomNote) {
+    if (!inventory[char]) {
+      return false;
+    }
+
+    inventory[char]--;
+  }
+  return true;
+};
+
+console.log(canConstruct(ransomNote, magazine));
